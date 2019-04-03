446694950501d8c26c5313199b873f75c062e278
zhuyaoZY <1762045191@qq.com>
2019-04-03 19:56:09 +0800

    Site updated: 2019-04-03 19:56:05

---
+++ js/src/crash_cheat.js
@@ -0,0 +1,23 @@
+/*
+* @Author: 不忘初心
+* @Date:   2019-04-03 19:53:51
+* @Last Modified by:   不忘初心
+* @Last Modified time: 2019-04-03 19:54:22
+*/
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
